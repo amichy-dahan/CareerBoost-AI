db155d707d6c7397fe75883472821f0b43b47cd1
amichy-dahan <amichyd1@gmail.com>
2025-09-21 17:53:50 +0300

    גגב

--- server/server.js
+++ server/server.js
@@ -44,8 +44,8 @@ app.get("/",(req, res)=>{
 
 async function startDB() {
   await mongoose.connect(process.env.MONGO_URI);
-  app.listen(process.env.PORT, () => {
-    console.log(`Server running on http://localhost:${process.env.PORT}`);
+  app.listen(3000, () => {
+    console.log(`Server running on http://localhost:3000`);
   });
 }
 
